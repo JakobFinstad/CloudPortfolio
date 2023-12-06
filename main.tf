20d7b7728d9423fd4b49f08980f4fb7ec00c1432
JakobFin <jakobf44@gmail.com>
2023-12-06 19:49:08 +0100

    WIP terraform workflow

--- main.tf
+++ main.tf
@@ -3,11 +3,37 @@
 provider "azurerm" {
   features{}
 }
+resource "random_string" "resource_code" {
+  length  = 5
+  special = false
+  upper   = false
+}
 
 resource "azurerm_resource_group" "CloudPortfolio_rg" {
   name     = "CloudPortfolio"
   location = "West Europe"
 }
+
+resource "azurerm_storage_account" "tfstate" {
+  name                     = "tfstate${random_string.resource_code.result}"
+  resource_group_name      = azurerm_resource_group.CloudPortfolio_rg.name
+  location                 = azurerm_resource_group.CloudPortfolio_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  allow_nested_items_to_be_public = false
+
+  tags = {
+    environment = "staging"
+  }
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.tfstate.name
+  container_access_type = "private"
+}
+
+
 resource "azurerm_virtual_network" "CloudPortfolio_vnet" {
   name                = "CloudPortVNet"
   address_space       = ["10.0.0.0/16"]
