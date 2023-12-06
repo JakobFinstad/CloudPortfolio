782dcef6b54339047858bfc83b5215090dd924b3
JakobFin <jakobf44@gmail.com>
2023-12-06 19:57:34 +0100

    WIP terraform workflow

--- main.tf
+++ main.tf
@@ -1,5 +1,19 @@
 # main.tf
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+  backend "azurerm" {
+      resource_group_name  = "CloudPortfolio"
+      storage_account_name = "tfstateq1nmo"
+      container_name       = "tfstate"
+      key                  = "terraform.tfstate"
+  }
 
+}
 provider "azurerm" {
   features{}
 }
@@ -8,6 +22,7 @@ resource "random_string" "resource_code" {
   special = false
   upper   = false
 }
+
 
 resource "azurerm_resource_group" "CloudPortfolio_rg" {
   name     = "CloudPortfolio"
