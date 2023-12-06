f9bedaab2e472c1357ddd4ec365ded21405e9fb2
JakobFin <jakobf44@gmail.com>
2023-12-06 14:22:55 +0100

    Added resources to the terraform

--- main.tf
+++ main.tf
@@ -1,10 +1,75 @@
 # main.tf
 
 provider "azurerm" {
-  features = {}
+  features{}
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "cloud-shell-storage-westeurope"
-  location = "westeurope"
+resource "azurerm_resource_group" "CloudPortfolio_rg" {
+  name     = "CloudPortfolio"
+  location = "West Europe"
+}
+resource "azurerm_virtual_network" "CloudPortfolio_vnet" {
+  name                = "CloudPortVNet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.CloudPortfolio_rg.location
+  resource_group_name = azurerm_resource_group.CloudPortfolio_rg.name
+}
+
+resource "azurerm_subnet" "CloudPortfolio_subnet" {
+  name                 = "CloudPortSubnet"
+  resource_group_name  = azurerm_resource_group.CloudPortfolio_rg.name
+  virtual_network_name = azurerm_virtual_network.CloudPortfolio_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "CloudPortfolio_nsg" {
+  name                = "CloudPortNSG"
+  location            = azurerm_resource_group.CloudPortfolio_rg.location
+  resource_group_name = azurerm_resource_group.CloudPortfolio_rg.name
+}
+
+resource "azurerm_network_security_rule" "CloudPortfolio_nsg_rule" {
+  name                        = "HTTP"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.CloudPortfolio_rg.name
+  network_security_group_name = azurerm_network_security_group.CloudPortfolio_nsg.name
+
+}
+
+resource "azurerm_subnet_network_security_group_association" "CloudPortfolio_subnet_nsg_association" {
+  subnet_id                 = azurerm_subnet.CloudPortfolio_subnet.id
+  network_security_group_id = azurerm_network_security_group.CloudPortfolio_nsg.id
+}
+
+
+resource "azurerm_service_plan" "CloudPortfolio_asp" {
+  name                = "CloudPortAppServicePlan"
+  location            = azurerm_resource_group.CloudPortfolio_rg.location
+  resource_group_name = azurerm_resource_group.CloudPortfolio_rg.name
+
+  os_type   = "Linux"   
+  sku_name  = "B1"      
+}
+
+resource "azurerm_app_service" "CloudPortfolio_app"{
+  name                = "CloudPortAppService"
+  location            = azurerm_resource_group.CloudPortfolio_rg.location
+  resource_group_name = azurerm_resource_group.CloudPortfolio_rg.name
+  app_service_plan_id = azurerm_service_plan.CloudPortfolio_asp.id
+
+  site_config {
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "LocalGit"
+  }
+
+  app_settings = {
+    "SOME_KEY" = "some-value"
+  }
 }
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import unittest
-from app import app, redis
+from app import app
 
 class TestApp(unittest.TestCase):
 
@@ -14,17 +14,11 @@ class TestApp(unittest.TestCase):
         # Clean up the test context
         self.app_context.pop()
 
-    def test_database_connection(self):
-        # Ensure the connection to the Redis database is successful
-        response = self.app.get('/')
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(b'This webpage has been viewed', response.data)
-
     def test_web_service_connection(self):
         # Ensure the Flask app is running and responds to requests
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'This webpage has been viewed', response.data)
+        self.assertIn(b'Hellow world!', response.data)
 
 if __name__ == '__main__':
     unittest.main()
