3d93a1a703b4ced2acd2a672ec3e1fa05d16a363
JakobFin <jakobf44@gmail.com>
2023-12-07 23:20:30 +0100

    Added outbound network rule

--- main.tf
+++ main.tf
@@ -72,6 +72,20 @@ resource "azurerm_network_security_rule" "CloudPortfolio_nsg_rule" {
 
 }
 
+resource "azurerm_network_security_rule" "AllowOutbound" {
+  name                        = "AllowOutbound"
+  priority                    = 2000
+  direction                   = "Outbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "AzureCloud"
+  resource_group_name         = azurerm_resource_group.CloudPortfolio_rg.name
+  network_security_group_name = azurerm_network_security_group.CloudPortfolio_nsg.name
+}
+
 resource "azurerm_subnet_network_security_group_association" "CloudPortfolio_subnet_nsg_association" {
   subnet_id                 = azurerm_subnet.CloudPortfolio_subnet.id
   network_security_group_id = azurerm_network_security_group.CloudPortfolio_nsg.id
