20ae89ce2a8198bfd404b9ff51e3d7bacba50f12
JakobFin <jakobf44@gmail.com>
2023-12-07 13:54:05 +0100

    Added backend congfig for the terraform

--- backend.hcl
+++
@@ -1,8 +0,0 @@
-resource_group_name  = "CloudPortfolio"
-storage_account_name = "tfstateq1nmo"
-container_name       = "tfstate"
-key                  = "tfstateq1nmo"
-use_oidc             = true
-subscription_id      = "sub_id"
-client_id            = "client_id"
-tenant_id            = "tenant_id"
--- main.tf
+++ main.tf
@@ -1,85 +1,21 @@
 # main.tf
 
-resource "github_actions_secret" "sub_id" {
-  repository = "CloudPortfolio"
-  secret_name = "AZURE_SUBSCRIPTION_ID"
+terraform { 
+    backend "azurerm" { 
+        resource_group_name         = "CloudPortfolio" 
+        storage_account_name        = "portfoliotfstate" 
+        container_name              = "tfstate" 
+        key                         = "CloudPortfolio.tfstate"
+        use_oidc                    = true
+    } 
 }
-resource "github_actions_secret" "tenant_id" {
-  repository = "CloudPortfolio"
-  secret_name = "AZURE_TENANT_ID"
-}
-resource "github_actions_secret" "client_id" {
-  repository = "CloudPortfolio"
-  secret_name = "AZURE_CLIENT_ID"
-}
-
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>3.0"
-    }
-  }
-  backend "azurerm" {
-    resource_group_name  = "CloudPortfolio"
-    storage_account_name = "tfstateq1nmo"
-    container_name       = "tfstate"
-    key                  = "tfstateq1nmo"
-    use_oidc             = true
-    subscription_id      = "sub_id"
-    tenant_id            = "tenant_id"
-  }
-}
-
-data "terraform_remote_state" "foo" {
-  backend = "azurerm"
-  config = {
-    resource_group_name  = "CloudPortfolio"
-    storage_account_name = "tfstateq1nmo"
-    container_name       = "tfstate"
-    key                  = "Bue5ffNS4IjDJ9ehTIcnmTi6JmpSD9sLe7zTqyL+er1sLtyzhNR3xrCfxR9owNoGyPx2rnU179Um+AStzBCaYg=="
-    use_oidc             = true
-    subscription_id      = "sub_id"
-    tenant_id            = "tenant_id"
-  }
-}
-
-
-
-
 provider "azurerm" {
   features{}
 }
 
-resource "random_string" "resource_code" {
-  length  = 5
-  special = false
-  upper   = false
-}
-
-
 resource "azurerm_resource_group" "CloudPortfolio_rg" {
   name     = "CloudPortfolio"
   location = "West Europe"
-}
-
-resource "azurerm_storage_account" "tfstate" {
-  name                     = "tfstate${random_string.resource_code.result}"
-  resource_group_name      = azurerm_resource_group.CloudPortfolio_rg.name
-  location                 = azurerm_resource_group.CloudPortfolio_rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  allow_nested_items_to_be_public = false
-
-  tags = {
-    environment = "staging"
-  }
-}
-
-resource "azurerm_storage_container" "tfstate" {
-  name                  = "tfstate"
-  storage_account_name  = azurerm_storage_account.tfstate.name
-  container_access_type = "private"
 }
 
 
